14e9366ec30ac54764ae23c0b3c7d8fd0fc14bbc
cnopens <cnopens@163.com>
2020-07-25 00:31:53 +0800

    update src

---
+++ src/main/java/GradleTutorialsApplication.java
@@ -0,0 +1,10 @@
+/***
+ @Project Gradle_Tutorials
+ @Desc   <b>This is Description</b>
+ @author cnopens@gmail.com
+ @Date 2020/7/19 0019
+ @Version v1.0
+ @CopyRights Administrator
+ **/
+public class GradleTutorialsApplication {
+}
